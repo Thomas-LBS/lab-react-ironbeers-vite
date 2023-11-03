08775942585362943176585d0e05ecfb8341bbdc
Thomas-LBS <thomas.lebas26@gmail.com>
2023-11-03 11:45:04 +0100

    Navbar style active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,15 @@ import { NavLink } from "react-router-dom"
 function Navbar() {
     return (
         <nav className="Navbar">
-            <NavLink to="/">
+            <NavLink className="logo style-links" to="/">
                 <img className="logo" src="/src/assets/home-icon.png" />
             </NavLink>
+            <div className="links">
+            <NavLink className="style-links" to="/beers">All Beers</NavLink>
+            <NavLink className="style-links" to="/random-beer">Random Beer</NavLink>
+            <NavLink className="style-links" to="/new-beer">Add a Beer</NavLink>
+            </div>
+            <div></div>
         </nav>
     )
 }
